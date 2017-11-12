010890c76ce24ab173f68eab944b6efbff6e1347
H5zhangyi <154730619@qq.com>
2017-11-12 20:32:55 +0800

    详情逻辑

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -1,3 +1,15 @@
+'use strict';
+
+import util from '../../utils/index.js';
+import config from '../../utils/config.js';
+
+// WxParse HtmlFormater 用来解析content文本为小程序视图
+
+
+// 把html转为标准安全的格式
+
+let app = getApp();
+
 Page({
 
   /**
@@ -11,55 +23,34 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    /*
+    * 函数 `onLoad` 会在页面初始化时候加载运行，其内部的 `option` 是路由跳转过来后的参数对象。
+    * 我们从 `option` 中解析出文章参数 `contendId`，然后通过调用 `util` 中封装好的 `request` 函数来获取 `mock` 数据。 
+    */
+
+    let id = option.contentId || 0;
+    this.init(id);
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
+  init (contentId) {
+    if (contentId) {
+      this.requestDetail(contentId)
+        .then(data => {
+          util.log(data);
+        })
+    }
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
+  requestDetail (contentId) {
+    return util.request({
+      url: 'detail',
+      mock: true,
+      data: {
+        source: 1
+      }
+    })
+    .then(res => {
+      util.log(res);
+    })
   }
 })
